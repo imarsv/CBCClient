2b2673a6e49c788cd89bcacaaae48a82d6b3070c
Serhii Marchuk <serhii.marchuk@gmail.com>
2018-06-06 16:49:46 +0300

    Add node service

---
+++ src/app/service/node.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
+
+export class Node {
+  constructor(public id?: string,
+              public name?: string,
+              public connection?: NodeConnection) {
+  }
+}
+
+export class NodeConnection {
+  constructor(public protocol?: string,
+              public hostname?: string,
+              public port?: number) {
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NodeService {
+
+  constructor(private auth: AuthService, private httpClient: HttpClient) {
+  }
+
+  list() {
+    const url = `http://${location.hostname}:3000/nodes`;
+
+    return this.httpClient
+      .get<Node[]>(url, { headers: { 'Authorization': `Bearer ${this.auth.getToken()}` } });
+  }
+}
