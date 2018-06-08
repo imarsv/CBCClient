877ad13f279b3ff9e82747f9d042dfddde4fc6e0
Serhii Marchuk <serhii.marchuk@gmail.com>
2018-06-08 09:42:31 +0300

    Add clipboard service

---
+++ src/app/service/clipboard.service.ts
@@ -0,0 +1,57 @@
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Injectable()
+export class ClipboardService {
+
+  private dom: Document;
+
+  constructor(@Inject(DOCUMENT) dom: Document) {
+    this.dom = dom;
+  }
+
+  public copy(value: string): Promise<string> {
+    return new Promise((resolve, reject): void => {
+
+      let textarea = null;
+
+      try {
+
+        // In order to execute the "Copy" command, we actually have to have
+        // a "selection" in the currently rendered document. As such, we're
+        // going to inject a Textarea element and .select() it in order to
+        // force a selection.
+        // --
+        // NOTE: This Textarea is being rendered off-screen.
+        textarea = this.dom.createElement('textarea');
+        textarea.style.height = '0px';
+        textarea.style.left = '-100px';
+        textarea.style.opacity = '0';
+        textarea.style.position = 'fixed';
+        textarea.style.top = '-100px';
+        textarea.style.width = '0px';
+        this.dom.body.appendChild(textarea);
+
+        // Set and select the value (creating an active Selection range).
+        textarea.value = value;
+        textarea.select();
+
+        // Ask the browser to copy the current selection to the clipboard.
+        this.dom.execCommand('copy');
+
+        resolve(value);
+
+      } finally {
+
+        // Cleanup - remove the Textarea from the DOM if it was injected.
+        if (textarea && textarea.parentNode) {
+
+          textarea.parentNode.removeChild(textarea);
+
+        }
+
+      }
+
+    });
+  }
+}
